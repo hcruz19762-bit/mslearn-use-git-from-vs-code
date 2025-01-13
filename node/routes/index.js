cf1b91393b59512f074e27cf46896d9577073c80
hector cruz <hcruz19762@outlook.com>
2025-01-13 14:06:57 -0700

    change1

--- node/routes/index.js
+++ node/routes/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-var router = express.Router();
+var express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
